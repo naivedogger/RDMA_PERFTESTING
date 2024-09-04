90a058a798e7a371727143e76eaf56c5e460200e
Thomashu <Thomashu@>
2024-09-04 00:04:23 -0600

    NO_BATCHING

--- rdma-common.c
+++ rdma-common.c
@@ -1,6 +1,15 @@
 #include "rdma-common.h"
 
-static const int RDMA_BUFFER_SIZE = 1024;
+// 修改这个以后记得在服务器上也修改一下
+// 不batch的情况下，BUFFER_SIZE = 4096 的时候，WRITE 40MB需要的时间大概是3w us，
+// 同样不batch，size = 16 * 4096 的时候，WRITE 40MB需要的时间是14512 us
+// 预计后续实现了Doorbell Batching的话，还能比这个更快
+// 感觉这样可以初步验证“被动上锁”的想法，因为需要支持同步操作的话，那么resize需要 1~2 s 这样的时间
+// 如果上锁的话，虽然这个过程中不允许写和插入，但是整体处理起来要简单很多，而且可能也就是需要一小段时间(几千 us)
+// 有一个点需要考虑：那就是读kv。但是实际上也是可以批量读取的，因为上锁以后不允许修改
+// 但是还是需要重新设计一下插入和更新的流程，因为如果先检查锁（比如说在把桶读回来后发现没上锁，然后修改，但是可能在这个过程中触发了resize上锁，那么这个修改就有可能丢失）
+// 如果把 CAS 改成 WRITE 会不会好弄一些，因为不会失败
+static const int RDMA_BUFFER_SIZE = 4096 * 16;
 
 struct message {
   enum {
@@ -205,7 +214,7 @@ void on_completion(struct ibv_wc *wc)
   if (conn->send_state == SS_MR_SENT && conn->recv_state == RS_MR_RECV) {
     //struct ibv_send_wr wr, 
     struct ibv_send_wr *bad_wr = NULL;
-    struct ibv_sge sge;
+    // struct ibv_sge sge;
 
     if (s_mode == M_WRITE)
       printf("received MSG_MR. writing message to remote memory...\n");
@@ -227,42 +236,55 @@ void on_completion(struct ibv_wc *wc)
     // wr.wr.rdma.remote_addr = (uintptr_t)conn->peer_mr.addr;
     // wr.wr.rdma.rkey = conn->peer_mr.rkey;
 
-    sge.addr = (uintptr_t)conn->rdma_local_region;
-    sge.length = RDMA_BUFFER_SIZE;
-    sge.lkey = conn->rdma_local_mr->lkey;
     // bulky write， 10K 轮
     struct timeval start, end;
     gettimeofday(&start, NULL);
     // int j = 0;
-    for(int i = 0; i < 10000; ){
-      struct ibv_send_wr wr[16];
-      int k = 16;
-      for(int j = 0; j < k && i < 10000; j++){
-        wr[j].next = (j == k - 1) ? NULL : &wr[j + 1];
+    struct ibv_cq *cq = NULL;
+    void *ctx = NULL;
+    TEST_NZ(ibv_get_cq_event(s_ctx->comp_channel, &cq, &ctx));
+    ibv_ack_cq_events(cq, 1);
+    TEST_NZ(ibv_req_notify_cq(cq, 0));
+    const int k = 1;
+    struct ibv_send_wr wr[k];
+    struct ibv_sge sg[k];
+    const int rounds = 40 * 1024 * 1024 / RDMA_BUFFER_SIZE;
+    for(int i = 0; i < rounds; ){
+      // k = 1 的时候是没有问题的，结果大概是 3w us，但是 k = 16 就不行了
+      // 怀疑：每个wr都要有自己的sge？
+      for(int j = 0; j < k && i < rounds; j++){
+        memset(&sg[j], 0, sizeof(struct ibv_sge));
+        memset(&wr[j], 0, sizeof(struct ibv_send_wr));
+
+        sg[j].addr = (uintptr_t)conn->rdma_local_region;
+        sg[j].length = RDMA_BUFFER_SIZE;
+        sg[j].lkey = conn->rdma_local_mr->lkey;
+
+        wr[j].next = (j == k - 1 || i == rounds - 1) ? NULL : &wr[j + 1];
         wr[j].wr_id = (uintptr_t)conn;
         wr[j].opcode = (s_mode == M_WRITE) ? IBV_WR_RDMA_WRITE : IBV_WR_RDMA_READ;
-        wr[j].sg_list = &sge;
+        wr[j].sg_list = &sg[j];
         wr[j].num_sge = 1;
-        wr[j].send_flags = IBV_SEND_SIGNALED;
+        // 发现好像是这里的标志设置的不对
+        if(j == k - 1)
+          wr[j].send_flags = IBV_SEND_SIGNALED;
         wr[j].wr.rdma.remote_addr = (uintptr_t)conn->peer_mr.addr;
         wr[j].wr.rdma.rkey = conn->peer_mr.rkey;
         i ++;
       }
       //需要研究一下怎么把这个弄成一个batch，每次只发一条、poll一条还是太慢了，而且还会报错
       //这样的时间大致是0.46s
+      // 妈的这里返回值12代表什么啊
       int rc = ibv_post_send(conn->qp, &wr[0], &bad_wr);
       struct ibv_wc wc;
       //这个cq应该不是通过channel获得的吧。。。
       //channel的cq应该是建立连接的时候用的，我估计得自己搞一个，因为它原来的代码没有poll
-      //但是创建qp的时候感觉是给了这个cq的，按理来说没问题呀
-      struct ibv_cq *cq = NULL;
-      void *ctx = NULL;
-      TEST_NZ(ibv_get_cq_event(s_ctx->comp_channel, &cq, &ctx));
-      ibv_ack_cq_events(cq, 1);
-      TEST_NZ(ibv_req_notify_cq(cq, 0));
+      //但是创建qp的时候是给了这个cq的，按理来说没问题呀
       printf("rc = %d\n",rc);
       assert(rc == 0);
       int new_cnt = 0, cnt = 0;
+      // 加上这一行也没啥用，难道说不是CQ的问题吗
+      cq = conn->qp->send_cq;
       do {
 
         new_cnt = ibv_poll_cq(cq,1,&wc);
@@ -276,7 +298,7 @@ void on_completion(struct ibv_wc *wc)
         if(wc.status != IBV_WC_SUCCESS){
           exit(-1);
         }
-        printf("cq_cnt: %d\n",new_cnt);
+        //printf("cq_cnt: %d\n",new_cnt);
 
         // while (ibv_poll_cq(cq, 1, &wc)){
         //   f = 0;
